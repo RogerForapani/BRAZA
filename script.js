4451849a84a2f07bd12b759cb31d59635c082995
Roger Forapani <109592738+RogerForapani@users.noreply.github.com>
2024-11-16 19:43:48 -0300

    final commit.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ async function fetchTokenData() {
   // Função que ajusta a quantidade de brasas com base no preço
   function updateFireBasedOnPrice(price) {
     const fireContainer = document.getElementById("fire-container");
-    const fireCount = Math.min(10000000, Math.max(10, Math.floor(price * 5))); // Calcula o número de brasas
+    const fireCount = Math.min(10000000, Math.max(10, Math.floor(price * 2))); // Calcula o número de brasas
   
     // Gera brasas extras se o preço aumentar
     const colors = ["yellow", "green", "blue"];
